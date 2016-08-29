6181b187c489adec57fd0cb293b176da6aa4569a
amaltesh <maltesh@usc.edu>
2016-08-29 00:59:12 -0700

    added files

---
+++ ShimmerGraphandLogService/src/com/shimmerresearch/shimmergraphandlogservice/ConstantsAWS.java
@@ -0,0 +1,16 @@
+package com.shimmerresearch.shimmergraphandlogservice;
+
+public class ConstantsAWS {
+	   /*
+     * You should replace these values with your own. See the README for details
+     * on what to fill in.
+     */
+    public static final String COGNITO_POOL_ID = "us-west-2:2f0eaa61-ab8a-4833-ac17-146544eae0ed";
+
+    /*
+     * Note, you must first create a bucket using the S3 console before running
+     * the sample (https://console.aws.amazon.com/s3/). After creating a bucket,
+     * put it's name in the field below.
+     */
+    public static final String BUCKET_NAME = "shimmerobjs";
+}
